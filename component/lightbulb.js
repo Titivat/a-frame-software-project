65447bca3650d887c27eeeb95c7384375a569450
titivat <titivatmo@gmail.com>
2021-12-19 19:43:28 +0700

    add lightblub that is drag and clicable

--- component/lightbulb.js
+++ component/lightbulb.js
@@ -1,14 +1,17 @@
 const lightBulb = document.createElement("a-entity");
 lightBulb.setAttribute("id", "IAmLight");
 // lightBulb.setAttribute("gltf-model", "#lightbulb");
-lightBulb.setAttribute("gltf-model", "https://cdn.aframe.io/examples/ar/models/reticle/reticle.gltf");
+lightBulb.setAttribute(
+	"gltf-model",
+	"https://raw.githubusercontent.com/Titivat/a-frame-software-project/main/Assets/3dObject/Lightbulb.gltf"
+);
 lightBulb.setAttribute("dragndrop", "");
 lightBulb.setAttribute("lightbulb", "");
 lightBulb.setAttribute(
-    "light",
-    "type: point; intensity: 1; distance: 100; decay: 0"
+	"light",
+	"type: point; intensity: 1; distance: 100; decay: 0"
 );
 lightBulb.setAttribute("position", "0 2 0");
-lightBulb.setAttribute("scale", "1 1 1");
+lightBulb.setAttribute("scale", "0.001 0.001 0.001");
 lightBulb.setAttribute("color", "#4CC3D9");
 scene.appendChild(lightBulb);
