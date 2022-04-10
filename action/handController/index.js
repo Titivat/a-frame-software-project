e5cc9d554583d05bf8c0dac5517766e3dcce92f6
titivat <titivatmo@gmail.com>
2022-04-10 20:04:57 +0700

    add vr press x button

--- action/handController/index.js
+++ action/handController/index.js
@@ -1,17 +1,23 @@
 AFRAME.registerComponent("create-world-menu", {
-	schema: {
-		isWorldMenuOpen: false,
-	},
 	init: function () {
-		this.el.addEventListener("abuttondown", this.aButtonPress);
+		this.isWorldMenuOpen = false;
+		this.el.addEventListener("xbuttondown", this.xButtonPress);
 	},
-	aButtonPress: function (evt) {
+	xButtonPress: function (evt) {
 		if (this.isWorldMenuOpen) {
 			console.log("I am true");
 			this.isWorldMenuOpen = false;
 		} else {
 			console.log("I am false");
 			this.isWorldMenuOpen = true;
+			const box = document.createElement("a-entity");
+			box.setAttribute(
+				"geometry",
+				"primitive: box; width: 1; height: 1; depth: 1;"
+			);
+			box.setAttribute("position", "0 2 -3");
+			const scene = document.querySelector("a-scene");
+			scene.appendChild(box);
 		}
 	},
 });
